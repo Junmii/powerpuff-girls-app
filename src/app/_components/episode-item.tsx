af12a56315e92b5e902e614eea2f3b15c1ff689a
Junmii <joys.it.projects@gmail.com>
2025-05-28 21:07:20 +0200

    Fixed forgotten stray url

--- src/app/_components/episode-item.tsx
+++ src/app/_components/episode-item.tsx
@@ -1,11 +1,12 @@
 ﻿import styles from './episode-item.module.scss'
 import Link from "next/link";
+import placeholder from "@/app/_assets/ppg-placeholder.jpg"
 
 export default function EpisodeItem({episode}: any) {    
     return (
         <Link className={styles.episodeItemContainer} href={`/episodes/${episode.id}`}>
             <div>
-                <img className={styles.episodeImage} src={episode.image?.medium ?? "https://occ-0-8407-90.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABVloo8iF8V3tS7pbvXxAfbuNRAgoNheZwgzaofxSBfPox5TPF0JYetktz4pD7nbRboHdhMB_95AjuouJDI0TBJC4AV5ha_uTQX_i.jpg?r=2f5"} alt="episode"/>
+                <img className={styles.episodeImage} src={episode.image?.medium ?? placeholder.src} alt="episode"/>
                 <strong>{episode.name}</strong>
             </div>
         </Link>
